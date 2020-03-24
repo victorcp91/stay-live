ceaba69e53a7e49f15b1999a26745f0ff7c66c37
Victor Correa <victor.correapereira@gmail.com>
2020-03-24 10:58:06 -0300

    try env vars again

--- next.config.js
+++ next.config.js
@@ -1,10 +1,35 @@
 const { parsed: localEnv } = require("dotenv").config();
 const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+const path = require("path");
 
 module.exports = {
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 
+    config.node = { fs: "empty" };
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, ".env"),
+        systemvars: true
+      })
+    ];
+
     return config;
+  },
+  publicRuntimeConfig: {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
   }
 };
