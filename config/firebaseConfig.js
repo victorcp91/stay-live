084914e617e3e83cb2777403006a28c4c1da47d3
Victor Correa <victor.correapereira@gmail.com>
2020-03-24 09:57:48 -0300

    hide env vars

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,12 +1,23 @@
+const {
+  apiKey,
+  authDomai,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDQ6BOQwFgotSw2Fq9ZHQm9mvK0mVsPErg",
-  authDomain: "stay-live.firebaseapp.com",
-  databaseURL: "https://stay-live.firebaseio.com",
-  projectId: "stay-live",
-  storageBucket: "stay-live.appspot.com",
-  messagingSenderId: "644557959892",
-  appId: "1:644557959892:web:99d907e7f69dbbe93c4141",
-  measurementId: "G-4PELRTB8RK"
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId
 };
 
 export default firebaseConfig;
