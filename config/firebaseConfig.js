590406f79862ec958f033e5c3112f3f48cd11256
Victor Correa <victor.correapereira@gmail.com>
2020-03-24 10:05:21 -0300

    fix env vars

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDQ6BOQwFgotSw2Fq9ZHQm9mvK0mVsPErg",
-  authDomain: "stay-live.firebaseapp.com",
-  databaseURL: "https://stay-live.firebaseio.com",
-  projectId: "stay-live",
-  storageBucket: "stay-live.appspot.com",
-  messagingSenderId: "644557959892",
-  appId: "1:644557959892:web:99d907e7f69dbbe93c4141",
-  measurementId: "G-4PELRTB8RK"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 export default firebaseConfig;
