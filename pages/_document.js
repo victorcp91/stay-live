113e0ebc8ad699f963c3b62ebfd37c4a3a943170
Victor Correa <victor.correapereira@gmail.com>
2020-03-29 22:04:18 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import styled from 'styled-components';
 import '../config/firebaseConfig';
 
 class MyDocument extends Document {
@@ -11,35 +12,31 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
-        <style jsx global>{`
-          html,
-          body {
-            margin: 0 !important;
-            padding: 0 !important;
-            background-color: #eaf4f4;
-            font-family: 'Roboto', sans-serif;
-          }
-
-          * {
-            box-sizing: border-box;
-          }
-          button {
-            cursor: pointer;
-            outline: none;
-          }
-        `}</style>
-        <body>
+        <Body>
           <Main />
           <NextScript />
-
           <link
             href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap"
             rel="stylesheet"
           ></link>
-        </body>
+        </Body>
       </Html>
     );
   }
 }
+
+const Body = styled.body`
+  margin: 0 !important;
+  padding: 0 !important;
+  background-color: #eaf4f4;
+  font-family: 'Roboto', sans-serif;
+  * {
+    box-sizing: border-box;
+  }
+  button {
+    cursor: pointer;
+    outline: none;
+  }
+`;
 
 export default MyDocument;
